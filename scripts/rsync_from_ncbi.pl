1fccaf0f5ea5217978c1cb9918ac90e615fb66a5
Thomas A. Christensen II <25492070+MillironX@users.noreply.github.com>
2021-06-16 14:48:16 -0500

    Allow rsync failures when NCBI removes refseq genomes

--- scripts/rsync_from_ncbi.pl
+++ scripts/rsync_from_ncbi.pl
@@ -119,7 +119,7 @@ if ($use_ftp) {
 }
 else {
   print STDERR "Step 1/2: Performing rsync file transfer of requested files\n";
-  system("rsync --no-motd --files-from=manifest.txt rsync://${SERVER}${SERVER_PATH}/ .") == 0
+  system("rsync --no-motd --files-from=manifest.txt rsync://${SERVER}${SERVER_PATH}/ .")
     or die "$PROG: rsync error, exiting: $?\n";
   print STDERR "Rsync file transfer complete.\n";
 }
